d97aa70913e11e8ead0b9608e57b99de3e3b246c
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-28 14:59:35 -0300

    feat: list users NeDB

--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,22 @@ let db = new NeDB({
 
 module.exports = (app) => {
   app.get("/users", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "aplication/json");
-    res.json({
-      users: [
-        {
-          name: "Lucas",
-          email: "lucas@gmail.com",
-          id: 1,
-        },
-      ],
-    });
+    db.find({})
+      .sort({ name: 1 })
+      .exec((err, users) => {
+        if (err) {
+          console.log(`error: ${err}`);
+          res.status(400).json({
+            error: err,
+          });
+        } else {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "aplication/json");
+          res.json({
+            users,
+          });
+        }
+      });
   });
 
   app.post("/users", (req, res) => {
@@ -24,10 +29,10 @@ module.exports = (app) => {
       if (err) {
         console.log(`error: ${err}`);
         res.status(400).json({
-            error: err
+          error: err,
         });
       } else {
-        res.status(200).json(user)
+        res.status(200).json(user);
       }
     });
   });
