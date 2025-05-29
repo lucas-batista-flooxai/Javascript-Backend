6b6cb67feb4d8770e7caddc2c9b7e5eebda1408b
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-29 12:12:16 -0300

    refactor: validator for put and post

--- routes/users.js
+++ routes/users.js
@@ -24,15 +24,7 @@ module.exports = (app) => {
   });
 
   route.post((req, res) => {
-    req.assert("name", "O nome é obrigatório.").notEmpty();
-    req.assert("email", "O e-mail está inválido.").notEmpty().isEmail();
-
-    let errors = req.validationErrors();
-
-    if (errors) {
-      app.utils.error.send(errors, req, res);
-      return false;
-    }
+    if (app.utils.validator.user(app, req, res)) return false;
 
     db.insert(req.body, (err, user) => {
       if (err) {
@@ -56,15 +48,7 @@ module.exports = (app) => {
   });
 
   routeId.put((req, res) => {
-    req.assert("name", "O nome é obrigatório.").notEmpty();
-    req.assert("email", "O e-mail está inválido.").notEmpty().isEmail();
-
-    let errors = req.validationErrors();
-
-    if (errors) {
-      app.utils.error.send(errors, req, res);
-      return false;
-    }
+    if (app.utils.validator.user(app, req, res)) return false;
 
     db.update({ _id: req.params.id }, req.body, (err) => {
       if (err) {
