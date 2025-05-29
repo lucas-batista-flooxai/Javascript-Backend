e8539625e73ad8b8b4e31c144e049a4b4456b8d4
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-29 11:10:58 -0300

    feat: add route get user data

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,8 @@ let db = new NeDB({
 });
 
 module.exports = (app) => {
+  let route = app.route("/users");
 
-  let route = app.route('/users');
-    
   route.get((req, res) => {
     db.find({})
       .sort({ name: 1 })
@@ -26,6 +25,18 @@ module.exports = (app) => {
 
   route.post((req, res) => {
     db.insert(req.body, (err, user) => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user);
+      }
+    });
+  });
+
+  let routeId = app.route("/users/:id");
+
+  routeId.get((req, res) => {
+    db.findOne({ _id: req.params.id }).exec((err, user) => {
       if (err) {
         app.utils.error.send(err, req, res);
       } else {
